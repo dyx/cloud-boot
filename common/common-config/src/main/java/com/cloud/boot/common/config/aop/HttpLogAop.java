7ca6739fa371c0574e51f97353f320444f898719
lhd <>
2025-03-19 08:29:30 +0800

    重构请求日志

--- common/common-config/src/main/java/com/cloud/boot/common/config/aop/HttpLogAop.java
+++ common/common-config/src/main/java/com/cloud/boot/common/config/aop/HttpLogAop.java
@@ -3,11 +3,9 @@ package com.cloud.boot.common.config.aop;
 import com.cloud.boot.common.core.jackson.JacksonUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -22,55 +20,37 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Aspect
 public class HttpLogAop {
 
-    private final ThreadLocal<Long> initRequestTime = new ThreadLocal<>();
-
     @Value("${custom.http.log.enable:true}")
     private Boolean enabled;
 
-    @Pointcut("execution(public * com.cloud.boot..controller.*Controller.*(..))")
-    private void point() {
-    }
-
-    @Before("point()")
-    public void before(JoinPoint joinPoint) {
-
+    @Around("execution(public * com.cloud.boot..controller.*Controller.*(..))")
+    public Object logRequest(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object result;
         if (enabled) {
-            try {
-                initRequestTime.set(System.currentTimeMillis());
 
-                HttpServletRequest request = getRequest();
+            long startTime = System.currentTimeMillis();
+            result = joinPoint.proceed();
+            long duration = System.currentTimeMillis() - startTime;
 
-                log.info("******************** HTTP Request Start ********************");
-                log.info("url：{}", request.getRequestURI());
-                log.info("method：{}", request.getMethod());
-                log.info("classMethod：{}.{}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
-                log.info("params：{}", JacksonUtil.toJsonStr(joinPoint.getArgs()));
-                log.info("******************** HTTP Request End ********************");
+            HttpServletRequest request = getRequest();
+            String method = request != null ? request.getMethod() : "";
+            String requestUrl = request != null ? request.getRequestURL().toString() : "";
+            String queryString = request != null ? request.getQueryString() : "";
+            String fullUrl = requestUrl + (queryString != null ? "?" + queryString : "");
 
-            } catch (Exception e) {
-                log.error("记录Http请求日志出错", e);
-            }
+            log.info("******************** HTTP Log Start ********************");
+            log.info("{} {}", method, fullUrl);
+            log.info("classMethod：{}.{}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+            log.info("params：{}", JacksonUtil.toJsonStr(joinPoint.getArgs()));
+            log.info("body：{}", JacksonUtil.toJsonStr(result));
+            log.info("time：{}ms", duration);
+            log.info("******************** HTTP Log End ********************");
         }
-    }
-
-    @AfterReturning(returning = "obj", pointcut = "point()")
-    public void afterReturning(Object obj) {
-
-        if (enabled) {
-            try {
-                HttpServletRequest request = getRequest();
-                log.info("******************** HTTP Response Start ********************");
-                log.info("url：{}", request.getRequestURI());
-                log.info("method：{}", request.getMethod());
-                log.info("body：{}", JacksonUtil.toJsonStr(obj));
-                log.info("耗时：{}ms", (System.currentTimeMillis() - initRequestTime.get()));
-                log.info("******************** HTTP Response End ********************");
-                // 防止线程复用造成的数据混乱
-                initRequestTime.remove();
-            } catch (Exception e) {
-                log.error("记录Http响应日志出错", e);
-            }
+        else {
+            result = joinPoint.proceed();
         }
+
+        return result;
     }
 
     public static HttpServletRequest getRequest() {
@@ -78,7 +58,7 @@ public class HttpLogAop {
             RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
             return ((ServletRequestAttributes) requestAttributes).getRequest();
         }
-        catch (Exception e) {}
+        catch (Exception ignored) {}
         return null;
     }
 }
